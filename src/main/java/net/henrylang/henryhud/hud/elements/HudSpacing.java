ddf1e8d8b8996f20be053985e8e82c0c9844bfbf
Ashley1227 <ashley.rued1227@gmail.com>
2021-01-28 20:20:23 -0500

    Bugfixes

--- src/main/java/net/henrylang/henryhud/hud/elements/HudSpacing.java
+++
@@ -1,9 +0,0 @@
-package net.henrylang.henryhud.hud.elements;
-
-import net.henrylang.henryhud.hud.HudElement;
-
-public class HudSpacing extends HudElement {
-	public HudSpacing() {
-		prefix = null;
-	}
-}
